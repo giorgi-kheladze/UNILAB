4eeaae7ac1badc601b68c56553e4838afd8c90f2
Giorgi Kheladze <157839203+giorgi-kheladze@users.noreply.github.com>
2024-10-22 10:36:31 +0400

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,22 +1,25 @@
-class MainHeader extends HTMLElement{
-    connectedCallback(){
+class MainHeader extends HTMLElement {
+    connectedCallback() {
         this.innerHTML = `
             <header class="header-desktop">
                 <div class="frame47">
                     <div class="frame">
-                        <img src="images/Group.png">
-                        <p class="logo-text">Pets for Best</p>
+                        <a href="index.html">
+                            <img src="images/Group.png" alt="Group Image">
+                        </a>                        <p class="logo-text">Pets for Best</p>
                     </div>
                     <ul class="navlist">
                         <li><a href="index.html">Home</a></li>
                         <li><a href="category.html">Category</a></li>
                         <li><a href="contact.html">Contact</a></li>
                     </ul>
+                    <div class="burger-menu">
+                        <i class="bx bx-menu"></i>
+                    </div>
                 </div>
 
                 <div class="frame46">
                     <a href="login.html" class="join">Join the community</a>
-
                     <div class="frame44">
                         <img src="">
                         <p>GEO</p>
@@ -24,12 +27,38 @@ class MainHeader extends HTMLElement{
                     </div>
                 </div>
             </header>
-        `
+
+            <div class="modal" id="menuModal">
+                <div class="modal-content">
+                    <span class="close-button">&times;</span>
+                    <ul class="navlist">
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="category.html">Category</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                    <div class="frame46">
+                        <a href="login.html" class="join">Join the community</a>
+                        <div class="frame44">
+                            <img src="">
+                            <p>GEO</p>
+                            <i class='bx bx-chevron-down'></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        this.querySelector('.burger-menu').addEventListener('click', () => {
+            document.getElementById('menuModal').style.display = 'block';
+        });
+
+        this.querySelector('.close-button').addEventListener('click', () => {
+            document.getElementById('menuModal').style.display = 'none';
+        });
     }
 }
 
 customElements.define('main-header', MainHeader);
-
 
 class MainFooter extends HTMLElement{
     connectedCallback(){
@@ -39,11 +68,11 @@ class MainFooter extends HTMLElement{
                     <p>Register Now So You Don't Miss Our Programs</p>
                     <div class="email">
                         <input type="text" placeholder="Enter your Email" class="subtext">
-                        <a href="#" class="subscribe">Subscribe Now</a>
+                        <button class = 'subscribe'>Subscribe Now</button>
                     </div>
                 </div>
                 <div class="frame27">
-                    <ul class="navlist">
+                    <ul class="navlistforfoot">
                         <li><a href="">Home</a></li>
                         <li><a href="">Category</a></li>
                         <li><a href="#">About</a></li>
